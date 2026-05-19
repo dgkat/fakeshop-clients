5f61f778dc7bca3fcccb1b2ed4fb5bca9314a745
dgkat <dgkatoudis@gmail.com>
2026-05-20 02:23:10 +0300

    Create iosNotificationsModule

---
+++ composeApp/src/iosMain/kotlin/org/example/fakeshop_clients/features/notifications/di/IosNotificationsModule.kt
@@ -0,0 +1,9 @@
+package org.example.fakeshop_clients.features.notifications.di
+
+import org.example.fakeshop_clients.features.notifications.presentation.NotificationPrefsViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val iosNotificationsModule = module {
+    viewModel { NotificationPrefsViewModel() }
+}
